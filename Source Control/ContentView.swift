f45987d2b408590a4cf456a4b373971a95c3c310
Gidon Hallside <gidon.hallside@gmail.com>
2022-03-28 16:42:24 +0300

    Initial Commit

---
+++ Source Control/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Source Control
+//
+//  Created by Gidon Hallside on 28/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Source Control/Source_ControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  Source_ControlApp.swift
+//  Source Control
+//
+//  Created by Gidon Hallside on 28/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct Source_ControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
