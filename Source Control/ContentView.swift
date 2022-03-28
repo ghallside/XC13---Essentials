2da54d86c3d9ac795a10f713fd372961e0a0ac98
Gidon Hallside <gidon.hallside@gmail.com>
2022-03-28 17:25:57 +0300

    Fixed bug A

--- Source Control/ContentView.swift
+++ Source Control/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world Update!")
+        Text("Hello, world updated bug !")
             .padding()
     }
 }
